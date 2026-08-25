3b9b3157fb91635db2f5f424176679d42f59871f
Kartik Pawar <knpawar789@gmail.com>
2026-08-25 22:11:55 +0530

    feat: auth: refresh access token

---
+++ services/auth/src/controllers/refreshAccessToken.ts
@@ -0,0 +1,96 @@
+import { Request, Response, NextFunction } from "express";
+import { db } from "../db/index.js";
+import { eq } from "drizzle-orm";
+import {
+  AppError,
+  asyncHandler,
+  generateRefrehToken,
+  hashRefreshToken,
+  responseMessage,
+  signJwt,
+  STATUS_CODES,
+  successResponse,
+} from "@finflow/shared";
+import { users, sessions } from "../db/schema.js";
+
+export const refreshAccessToken = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const token = req.cookies.refreshToken;
+
+    if (!token) {
+      throw new AppError(
+        STATUS_CODES.UNAUTHORIZED,
+        responseMessage.AUTH.REFRESH_TOKEN_INVALID,
+      );
+    }
+
+    const refreshAccessTokenHash = hashRefreshToken(token);
+
+    const [session] = await db
+      .select()
+      .from(sessions)
+      .where(eq(sessions.refreshTokenHash, refreshAccessTokenHash))
+      .limit(1);
+
+    if (!session) {
+      throw new AppError(
+        STATUS_CODES.UNAUTHORIZED,
+        responseMessage.AUTH.REFRESH_TOKEN_INVALID,
+      );
+    }
+
+    if (session?.revokedAt) {
+      throw new AppError(
+        STATUS_CODES.UNAUTHORIZED,
+        responseMessage.AUTH.SESSION_REVOKED,
+      );
+    }
+
+    if (session?.expiresAt < new Date()) {
+      throw new AppError(
+        STATUS_CODES.BAD_REQUEST,
+        responseMessage.AUTH.SESSION_EXPIRED,
+      );
+    }
+
+    const [user] = await db
+      .select()
+      .from(users)
+      .where(eq(users.id, session.userId));
+
+    if (!user) {
+      throw new AppError(
+        STATUS_CODES.UNAUTHORIZED,
+        responseMessage.AUTH.REFRESH_TOKEN_INVALID,
+      );
+    }
+
+    const newRefreshToken = generateRefrehToken();
+    const newRefreshTokenHash = hashRefreshToken(newRefreshToken);
+
+    await db
+      .update(sessions)
+      .set({
+        refreshTokenHash: newRefreshTokenHash,
+        lastUsedAt: new Date(),
+      })
+      .where(eq(sessions.id, session.id));
+
+    res.cookie("refreshToken", newRefreshToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+
+    const accessToken = signJwt({
+      userId: user.id,
+      purpose: "auth_login",
+      role: user?.role,
+      email: user.email,
+    });
+    return successResponse(res, {
+      accessToken,
+    });
+  },
+);
--- services/auth/src/routes/auth.routes.ts
+++ services/auth/src/routes/auth.routes.ts
@@ -4,11 +4,13 @@ import { createUserSchema, loginSchema } from "../schemas";
 import { registerUser } from "../controllers/registerUser.js";
 import { verifyEmail } from "../controllers/emailVerification";
 import { loginUser } from "../controllers/loginUser";
+import { refreshAccessToken } from "../controllers/refreshAccessToken";
 
 const router = Router();
 
 router.post("/register", validateBody(createUserSchema), registerUser);
 router.get("/verify-email", verifyEmail);
 router.post("/login", validateBody(loginSchema), loginUser);
+router.post("/refresh-access-token", refreshAccessToken);
 
 export default router;
