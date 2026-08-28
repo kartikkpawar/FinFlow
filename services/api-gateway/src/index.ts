8ba0b93eacad011ba3ce59f4d7f30375bb4aec6c
Kartik Pawar <knpawar789@gmail.com>
2026-08-28 13:04:10 +0530

    feat: api-gateway: setup & auth service redirection

---
+++ services/api-gateway/src/index.ts
@@ -0,0 +1,58 @@
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import rateLimit from "express-rate-limit";
+import { createProxyMiddleware } from "http-proxy-middleware";
+import {
+  AppError,
+  errorHandler,
+  httpLogger,
+  logger,
+  successResponse,
+} from "@finflow/shared";
+
+const PORT = process.env.PORT || 3001;
+
+// Services config urls
+const AUTH_SERVICE_URL =
+  process.env.AUTH_SERVICE_URL || "http://localhost:3002";
+
+const app = express();
+
+app.use(helmet());
+app.use(cors());
+
+app.use(
+  rateLimit({
+    windowMs: 15 * 60 * 1000,
+    limit: 100,
+    standardHeaders: "draft-8",
+    legacyHeaders: false,
+  }),
+);
+
+app.use(httpLogger);
+
+app.get("/health", (req, res) => {
+  successResponse(res, { service: "auth-service" });
+});
+
+app.use(
+  "/auth",
+  createProxyMiddleware({
+    target: AUTH_SERVICE_URL,
+    changeOrigin: true,
+    pathRewrite: (path) => `/auth${path}`,
+  }),
+);
+
+app.use((_req, _res, next) => {
+  next(new AppError(400, "Route Not Found"));
+});
+
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+  logger.info(`AUTH-SERVICE: Listening on port: ${PORT}`);
+});
